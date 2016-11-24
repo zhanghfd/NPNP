a7cead79ac8654f22175e4d1415f4dc2532dcd15
zhanghfd <zhanghfd@fudan.edu.cn>
2016-11-24 08:22:24 +0800

    first commit

--- R/NPNP.R
+++ R/NPNP.R
@@ -1,13 +1,13 @@
 
-NPNP = function(training,test_biomarker=NULL,validation_proportion=0.5,n.validation=5,fpr_thre=NULL,transformation='simultaneous'){
+NPNP = function(train_validation,test_biomarker=NULL,tv_ratio=1,n.validation=5,fpr_thre=NULL,transformation='simultaneous'){
 
-  train_d = training[,1];
-  train_y = training[,-1];
+  train_d = train_validation[,1];
+  train_y = train_validation[,-1];
   test_y = test_biomarker;
 
   trained_parameter<-parameter_from_training(train_y,train_d,transformation);
 
-  NPNP_training_results<-NPNP_training(validation_proportion,n.validation,trained_parameter,train_y,train_d)
+  NPNP_training_results<-NPNP_training(tv_ratio,n.validation,trained_parameter,train_y,train_d)
 
   fpr=NPNP_training_results$fpr;
   fnr=NPNP_training_results$fnr;
@@ -25,7 +25,7 @@ NPNP = function(training,test_biomarker=NULL,validation_proportion=0.5,n.validat
 
     test_prediction<-NPNP_prediction(test_y,optimal_com,NPNP_training_results,fpr_thre)
 
-    return(list(fpr=fpr,fnr=fnr,auc=auc,predict_d=test_prediction));
+    return(list(fpr=fpr,fnr=fnr,auc=auc,H=H,predict_d=test_prediction));
 
   }
 }
